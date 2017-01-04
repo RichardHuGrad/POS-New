180ba3bafb665eda24326070ffab29cae134431b
Jack1232 <wqjyhggg@gmail.com>
2017-01-04 13:35:45 -0500

    change order number to time related

--- app/Controller/HomesController.php
+++ app/Controller/HomesController.php
@@ -901,7 +901,8 @@ class HomesController extends AppController {
 
             // update order no            
             $data['Order']['id'] = $order_id;
-            $data['Order']['order_no'] = str_pad($order_id, 5, rand(98753, 87563), STR_PAD_LEFT);
+            // Change to DateTime related $data['Order']['order_no'] = str_pad($order_id, 5, rand(98753, 87563), STR_PAD_LEFT);
+            $data['Order']['order_no'] = date('Ymdhis').$table;
             $this->Order->save($data, false);
         } else {
             $order_id = $Order_detail['Order']['id'];
