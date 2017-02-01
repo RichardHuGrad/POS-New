7cb81e76488053a4a5416186074848ff65750a60
Yu Huang <yu.huang@uoit.com>
2017-02-01 15:04:33 -0500

    fix merge print

--- app/Controller/HomesController.php
+++ app/Controller/HomesController.php
@@ -786,58 +786,6 @@ class HomesController extends AppController {
         $this->set(compact('Order_detail', 'cashier_detail', 'table_no', 'today'));
     }
 
-    public function donepayment() {
-
-        $this->layout = false;
-        $this->autoRender = NULL;
-
-        // pr($this->data); die;
-        // get all params
-        $order_id = $this->data['order_id'];
-        $table = $this->data['table'];
-        $type = $this->data['type'];
-        $paid_by = strtoupper($this->data['paid_by']);
-
-        $pay = $this->data['pay'];
-        $change = $this->data['change'];
-
-        // save order to database        
-        $data['Order']['id'] = $order_id;
-        if ($this->data['card_val'] and $this->data['cash_val'])
-            $data['Order']['paid_by'] = "MIXED";
-
-        elseif ($this->data['card_val'])
-            $data['Order']['paid_by'] = "CARD";
-
-        elseif ($this->data['cash_val'])
-            $data['Order']['paid_by'] = "CASH";
-
-        $data['Order']['table_status'] = 'P';
-
-        $data['Order']['paid'] = $pay;
-        $data['Order']['change'] = $change;
-        $data['Order']['is_kitchen'] = 'Y';
-
-        $data['Order']['is_completed'] = 'Y';
-
-        $data['Order']['card_val'] = $this->data['card_val'];
-        $data['Order']['cash_val'] = $this->data['cash_val'];
-        $data['Order']['tip_paid_by'] = $this->data['tip_paid_by'];
-        $data['Order']['tip'] = $this->data['tip_val'];
-
-        $this->loadModel('Order');
-        $this->Order->save($data, false);
-
-        // update popularity status
-        $this->loadModel('Cousine');
-        $this->Cousine->query("UPDATE cousines set `popular` = `popular`+1 where id in(SELECT (item_id) from order_items where order_id = '$order_id')");
-
-        // save all 
-        $this->Session->setFlash('Order successfully completed.', 'success');
-
-        echo true;
-        exit; //Modified by Yishou Liao @ Nov 29 2016
-    }
 
     public function makeavailable() {
         $this->layout = false;
