6d3a4b61ac80d9b60abd17fe6a685ee600e809e3
Yu Huang <yu.huang@uoit.net>
2017-03-15 16:02:08 -0400

    fix

--- app/Lib/PrintConfig.php
+++ app/Lib/PrintConfig.php
@@ -3,7 +3,7 @@
      * config file of printer
      */
     class PrintConfig {
-        public static $addressLine1 = array("content" => "3700 Midland Ave. #108", "offset_x" => "156");
+        public static $addressLine1 = array("content" => "3700 Midland Ave. #108", "offset_x" => "116");
         public static $addressLine2 = array("content" => "Scarborogh ON M1V 0B3", "offset_x" => "110");
         public static $phone = array("content" => "647-352-5333", "offset_x" => "156");
 
