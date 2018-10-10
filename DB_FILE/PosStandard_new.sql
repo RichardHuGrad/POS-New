0a5c30a4f688ff0f01b3dc6efcc7b0464b66a03a
unknown <xhu294@uwo.ca>
2018-10-10 11:19:09 -0400

    fix

--- DB_FILE/PosStandard_new.sql
+++ DB_FILE/PosStandard_new.sql
@@ -19,7 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `pos`
 --
-
+CREATE DATABASE IF NOT EXISTS `pos` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `pos`;
 -- --------------------------------------------------------
 
 --
@@ -966,6 +967,23 @@ ALTER TABLE `cashiers`
 --
 ALTER TABLE `order_items`
   ADD CONSTRAINT `FK_orders_items_orders` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  
+
+CREATE TABLE `remote_order_syncs` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `order_type` varchar(32) CHARACTER SET latin1 NOT NULL,
+ `order_id` int(11) NOT NULL,
+ `synced` TINYINT(1) NOT NULL,
+ `last_tm` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+ `record` TEXT NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+ALTER TABLE `admins` ADD `singlecut` TINYINT NOT NULL;
+ALTER TABLE `admins` ADD `net_last_sync_tm` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Time for access cloud server' AFTER `singlecut`, ADD `net_new_order` TINYINT NOT NULL COMMENT 'flag for has new order from cloud server' AFTER `net_last_sync_tm`;
+ALTER TABLE `admins` ADD `net_order_kitchen` TINYINT NOT NULL COMMENT 'Net Order send Sent to Kitchen or not. 0 not, 1 yes' AFTER `net_new_order`;
+ALTER TABLE `admins` ADD `net_takeout_kitchen` TINYINT NOT NULL AFTER `net_order_kitchen`, ADD `net_order_voice` TINYINT NOT NULL AFTER `net_takeout_kitchen`, ADD `touch_screen_sound` TINYINT NOT NULL AFTER `net_order_voice`, ADD `default_tip_after_tax` TINYINT NOT NULL AFTER `touch_screen_sound`, ADD `main_page_show_price` TINYINT NOT NULL AFTER `default_tip_after_tax`;
+ALTER TABLE `admins` ADD `show2nd` TINYINT NOT NULL COMMENT 'Show send language or not. 0 not, 1 yes' AFTER `main_page_show_price`;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
